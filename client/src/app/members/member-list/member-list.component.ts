aa3779250c274f53ba5c9307f725e233cd7c27c1
Manjur Alam <er.manjur.alam9213@gmail.com>
2025-02-09 06:47:48 +0530

    end of section 9

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembesService } from '../../_services/membes.service';
+import { Member } from '../../_models/member';
+import { MemberCardComponent } from "../member-card/member-card.component";
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [],
+  imports: [MemberCardComponent],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit {
 
+  private memberService = inject(MembesService);
+  members: Member[] = [];
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe({
+      next: members => {
+        this.members = members;
+        console.log(members);
+      }, error: error => { console.log(error); },
+      complete: () => { console.log('complete'); }
+    });
+  }
 }
